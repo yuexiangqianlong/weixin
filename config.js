134fc9468fe820fa99d9529867ba94f91b454218
“yuexiangqianlong <1641149355@qq.com>
2018-01-30 15:28:40 +0800

    填写config.js

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ var util = require('./libs/util');
 var wechat_file = path.join(__dirname,'./config/wechat.txt');
 var config = {
 	wechat:{
-		appID:'wx8bcf610f475d2789',
-		appSecret:'57202f02c67517ed6a65720bf8f1c4fe',
-		token:'yuhailong',
+		appID:'',
+		appSecret:'',
+		token:'',
 		getAccessToken:function(){
 			return util.readFileAsync(wechat_file);
 		},
