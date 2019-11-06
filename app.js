ba04014853cef4308fb140b2ada22f000a69b99b
yuexiangqianlong <1641149355@qq.com>
2019-11-06 15:28:00 +0800

    delete menu

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-
 'use strict'
 
 var Koa = require('koa');
-var generator= require('./wechat/generator');
+var generator = require('./wechat/generator');
 var config = require('./config');
 var weixin = require('./weixin');
 
 var app = new Koa();
-app.use(generator(config.wechat,weixin.reply))
-app.use(weixin.setMenu);//自定义菜单
+app.use(generator(config.wechat, weixin.reply))
+// app.use(weixin.setMenu);//自定义菜单
 app.listen(9001);
 
 console.log('Listening 9001')
